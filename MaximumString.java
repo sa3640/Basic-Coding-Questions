29649c489363bf72d32b2bb05427a29acf2ac6b4
sa3640 <sa3640@gmail.com>
2024-02-12 23:34:03 +0530

    retw

---
+++ MaximumString.java
@@ -0,0 +1,37 @@
+class aiwee{
+    public static char maximumString(String s) {
+
+    //    int n = s.length();
+
+       int count = 0;
+        char string[] = s.toCharArray(); 
+        char fix = (char)0;
+
+
+      for(char str : string)
+      {
+         if(count==0)
+          fix = str;
+
+          if(str==fix)
+            count++;
+
+            else
+             count--;
+
+      }
+
+      return fix;
+      
+    }       
+        public static void main(String [] args)
+        {
+           
+            String s = "rajat";
+
+           char result = maximumString(s);
+            
+           System.out.println(result);
+        }
+        
+    }
